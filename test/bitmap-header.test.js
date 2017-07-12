fb860cd310623a764e61d97d1d37b5c7024f8651
Anwar Montasir <anwarmontasir@gmail.com>
2017-07-12 16:47:28 -0700

    bitmap-header.js successful test

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -7,24 +7,21 @@ describe('bitmap header', () => {
 
     let buffer = null;
     before(() => {
-        // TODO: read './test/test-bitmap.bmp' into buffer variable
-        //buffer = new Buffer('./test-bitmap.bmp');
+        // DONE: read './test/test-bitmap.bmp' into buffer variable
         buffer = new Buffer(fs.readFileSync('./test/test-bitmap.bmp'));
     });
 
     it('has correct specs', () => {
-        // TODO: read the docs to figure out what these values should be
+        // DONE: read the docs to figure out what these values should be
         assert.ok(constants.PIXEL_OFFSET);
         assert.ok(constants.BITS_PER_PIXEL_OFFSET);
         assert.ok(constants.FILE_SIZE_OFFSET);
     });
 
     it('parses header data', () => {
-        // TODO: use the constants to populate the following properties
+        // DONE: use the constants to populate the following properties
         // on the BitmapHeader in its constructor
         const header = new BitmapHeader(buffer);
-        console.log('testBuf = '+header.testBuf);
-
         assert.equal(header.pixelOffset, 54);
         assert.equal(header.bitsPerPixel, 24);
         assert.equal(header.fileSize, 30054);
