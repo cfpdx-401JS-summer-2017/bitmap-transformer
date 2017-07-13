5f719c201d740d2ca67ce8951248ca72190c060a
Wendy Beck <webeck@gmail.com>
2017-07-12 17:37:32 -0700

    pushing to start travis

--- tests/bitmap-header.test.js
+++ tests/bitmap-header.test.js
@@ -9,14 +9,12 @@ describe('bitmap header', () => {
         // TODO: read './test/test-bitmap.bmp' into buffer variable
           buffer = fs.readFileSync('./tests/test-bitmap.bmp');
         });
-
     it('has correct specs', () => {
         // TODO: read the docs to figure out what these values should be
         assert.ok(constants.PIXEL_OFFSET);
         assert.ok(constants.BITS_PER_PIXEL_OFFSET);
         assert.ok(constants.FILE_SIZE_OFFSET);
     });
-
     it('parses header data', () => {
         // TODO: use the constants to populate the following properties
         // on the BitmapHeader in its constructor
