e21eabed82594a0f09af610441d40c593aaafae9
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-07-12 19:01:27 -0700

    Bitmap Transformer loop built but not functional or passing tests. Committing/pushing to go home.

--- tests/bitmap-transformer.test.js
+++ tests/bitmap-transformer.test.js
@@ -1,5 +1,7 @@
 const assert = require('assert');
 const fs = require('fs');
+const constants = require('../lib/bitmap-constants.js');
+const BitmapHeader = require('../lib/bitmap-header');
 const BitmapTransformer = require('../lib/bitmap-transformer');
 const invert = require('../lib/invert-transformer');
 
@@ -21,15 +23,15 @@ describe('bitmap file transformer', () => {
 
         // call .transform(), which will modify the buffer.
         // in this api, you pass in a transformation function
-        bitmap.transform(invert);
+        // bitmap.transform(invert);
 
         // after above step, the buffer has been modified
         // and is accessible via bitmap.buffer
 
         // read the output file we saved earlier as
         // the "standard" expected output file
-        const buffer = fs.readFileSync('./test/inverted-expected.bmp');
-        assert.deepEqual(bitmap.buffer, buffer);
+        const invertedBuffer = fs.readFileSync('./tests/inverted-expected.bmp');
+        assert.deepEqual(bitmap.buffer, invertedBuffer);
 
         // if you don't have a standard file yet, you could write it 
         // out by commenting above code, using code below and visually inspect
