2f30a013e122d72aa9cfaf194c30d377edcdde27
Wendy Beck <webeck@gmail.com>
2017-07-12 22:49:39 -0700

    passing buffers around like a champ

--- tests/bitmap-header.test.js
+++ tests/bitmap-header.test.js
@@ -6,20 +6,16 @@ const BitmapHeader = require('../lib/bitmap-header');
 describe('bitmap header', () => {
     let buffer = null;
     before(() => {
-        // TODO: read './test/test-bitmap.bmp' into buffer variable
         const header = new BitmapHeader();
         const path = './tests/test-bitmap.bmp';
         buffer = header.createBuf(path);
       });
     it('has correct specs', () => {
-        // TODO: read the docs to figure out what these values should be
         assert.ok(constants.PIXEL_OFFSET);
         assert.ok(constants.BITS_PER_PIXEL_OFFSET);
         assert.ok(constants.FILE_SIZE_OFFSET);
     });
     it('parses header data', () => {
-        // TODO: use the constants to populate the following properties
-        // on the BitmapHeader in its constructor
         const header = new BitmapHeader();
         const path = './tests/test-bitmap.bmp';
         header.findBitmapValues(buffer, path);
--- tests/bitmap-transformer.test.js
+++ tests/bitmap-transformer.test.js
@@ -2,7 +2,8 @@ const assert = require('assert');
 const fs = require('fs');
 const BitmapHeader = require('../lib/bitmap-header');
 const BitmapTransformer = require('../lib/bitmap-transformer');
-const buffer = require();
+const invert = require('../lib/invert-transformer');
+const grayscale = require('../lib/grayscale-transformer');
 
 describe('bitmap file transformer', () => {
     let buffer = null;
@@ -14,21 +15,21 @@ describe('bitmap file transformer', () => {
 
     // "pinning" test, or "snapshot" test
     it('test whole transform', () => {
-        // use the BitmapTransformer class,
-        // passing in the buffer from the file read
         const bitmap = new BitmapTransformer(buffer);
 
-        // call .transform(), which will modify the buffer.
-        // in this api, you pass in a transformation function
         bitmap.transform(invert);
+        bitmap.transform(grayscale);
+
+        // console.log('4: ',bitmap);
+
 
         // after above step, the buffer has been modified
         // and is accessible via bitmap.buffer
 
         // read the output file we saved earlier as
         // the "standard" expected output file
-        const testBuffer = fs.readFileSync('./tests/inverted-expected.bmp');
-        assert.deepEqual(bitmap.buffer, testBuffer);
+        // const testBuffer = fs.readFileSync('./tests/inverted-expected.bmp');
+        // assert.deepEqual(bitmap.buffer, testBuffer);
 
         // if you don't have a standard file yet, you could write it
         // out by commenting above code, using code below and visually inspect
