2de90174ee3d58acafff83f4487c4757e751ae0b
Wendy Beck <webeck@gmail.com>
2017-07-14 21:01:28 -0700

    gs and inv transformers are working

--- tests/bitmap-transformer.test.js
+++ tests/bitmap-transformer.test.js
@@ -1,38 +1,36 @@
-const fs = require('fs');
-const assert = require('assert');
-const invTransformer = require('../lib/invert-transformer');
-const gsTransformer = require('../lib/grayscale-transformer');
-const BitmapTransformer = require('../lib/bitmap-transformer');
-const bitmapUtilities = require('../tests/bitmapUtilities');
+const fs = require( 'fs' );
+const assert = require( 'assert' );
+const invTransformer = require( '../lib/invert-transformer' );
+const gsTransformer = require( '../lib/grayscale-transformer' );
+const BitmapTransformer = require( '../lib/bitmap-transformer' );
+const bitmapUtilities = require( '../tests/bitmapUtilities' );
 
 // 8 bits in a byte
-describe('bitmap file transformer', () => {
-    let buffer = null;
-    before(() => {
-      const path = './tests/test-bitmap.bmp';
-      buffer = bitmapUtilities.createBuffer(path);
-    });
-    // "pinning" test, or "snapshot" test
-    it('test whole transform', () => {
-      const bitmap = new BitmapTransformer(buffer);
+describe( 'bitmap file transformer', () => {
+  let buffer = null;
+  before( () => {
+    const path = './tests/test-bitmap.bmp';
+    buffer = bitmapUtilities.createBuffer( path );
+  } );
+  // "pinning" test, or "snapshot" test
+  it( 'test whole transform', () => {
+    const bitmap = new BitmapTransformer( buffer );
+    bitmap.changeColor( invTransformer );
 
-      // const grayscale = grayscaleTransformer.transform;
-      bitmap.changeColor(invTransformer);
+    // console.log('grayscaled: ',grayscaled);
+    // const testBuffer = fs.readFileSync('./tests/my-inverted-expected.bmp');
 
-      // console.log('grayscaled: ',grayscaled);
-      // const testBuffer = fs.readFileSync('./tests/my-inverted-expected.bmp');
+    // after above step, the buffer has been modified
+    // and is accessible via bitmap.buffer
 
-        // after above step, the buffer has been modified
-        // and is accessible via bitmap.buffer
+    // read the output file we saved earlier as
+    // the "standard" expected output file
+    const testBuffer = fs.readFileSync( './tests/inverted-expected.bmp' );
+    assert.deepEqual( bitmap.buffer, testBuffer );
 
-        // read the output file we saved earlier as
-        // the "standard" expected output file
-        // const testBuffer = fs.readFileSync('./tests/inverted-expected.bmp');
-        // assert.deepEqual(bitmap.buffer, testBuffer);
-
-        // if you don't have a standard file yet, you could write it
-        // out by commenting above code, using code below and visually inspect
-        // the file for correctness.
-        return fs.writeFileSync('./tests/test-inverted-expected.bmp', bitmap.buffer);
-    });
-});
+    // if you don't have a standard file yet, you could write it
+    // out by commenting above code, using code below and visually inspect
+    // the file for correctness.
+    // return fs.writeFileSync( './tests/test-inverted-expected.bmp', bitmap.buffer );
+  } );
+} );
--- tests/tranformers.test.js
+++ tests/tranformers.test.js
@@ -1,44 +1,40 @@
-const assert = require('assert');
-const invTransformer = require('../lib/invert-transformer');
-const gsTransformer = require('../lib/grayscale-transformer');
-const bitmapUtilities = require('../tests/bitmapUtilities');
+const assert = require( 'assert' );
+const invTransformer = require( '../lib/invert-transformer' );
+const gsTransformer = require( '../lib/grayscale-transformer' );
+const bitmapUtilities = require( '../tests/bitmapUtilities' );
 
-// each pixel is made up of 3 bytes: b, g, r
-describe('transformers', () => {
+describe( 'transformers', () => {
   let buffer = null;
-  before(() => {
-      const path = './tests/test-bitmap.bmp';
-      buffer = bitmapUtilities.createBuffer(path);
-  });
-  it('invert', () => {
+  before( () => {
+    const path = './tests/test-bitmap.bmp';
+    buffer = bitmapUtilities.createBuffer( path );
+  } );
+  it( 'invert', () => {
 
-    const transformed = invTransformer({
+    const transformed = invTransformer( {
       r: 34,
       g: 100,
       b: 205
-    });
+    } );
 
-    assert.deepEqual(transformed, {
+    assert.deepEqual( transformed, {
       r: 221,
       g: 155,
       b: 50
-    });
-  });
-  it('grayscale', () => {
-    // HINT: grayscale assigns the average of all three colors
-    // as the new value for each color
-    // const gray = new GrayscaleTransformer(buffer);
-    // grayscale.transform();
-    const transformed = gsTransformer({
+    } );
+  } );
+  it( 'grayscale', () => {
+
+    const transformed = gsTransformer( {
       r: 34,
       g: 100,
       b: 205
-    });
+    } );
 
-    assert.deepEqual(transformed, {
+    assert.deepEqual( transformed, {
       r: 113,
       g: 113,
       b: 113
-    });
-  });
-});
+    } );
+  } );
+} );
