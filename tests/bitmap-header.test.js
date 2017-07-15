43b8e468e65af1a140cee54be95f26a891a507af
Wendy Beck <webeck@gmail.com>
2017-07-15 00:19:21 -0700

    finally finished??!!?

--- tests/bitmap-header.test.js
+++ tests/bitmap-header.test.js
@@ -4,21 +4,21 @@ const BitmapHeader = require('../lib/bitmap-header');
 const bitmapUtilities = require('./bitmapUtilities');
 
 describe('bitmap header', () => {
-    let buffer = null;
-    before(() => {
-        const path = './tests/test-bitmap.bmp';
-        buffer = bitmapUtilities.createBuffer(path);
-      });
-    it('has correct specs', () => {
-        assert.ok(constants.PIXEL_OFFSET);
-        assert.ok(constants.BITS_PER_PIXEL_OFFSET);
-        assert.ok(constants.FILE_SIZE_OFFSET);
-    });
-    it('parses header data', () => {
-        const header = new BitmapHeader(buffer);
-        header.findBitmapValues(buffer);
-        assert.equal(header.pixelOffset, 54);
-        assert.equal(header.bitsPerPixel, 24);
-        assert.equal(header.fileSize, 30054);
-    });
+  let buffer = null;
+  before(() => {
+    const path = './tests/test-bitmap.bmp';
+    buffer = bitmapUtilities.createBuffer(path);
+  });
+  it('has correct specs', () => {
+    assert.ok(constants.PIXEL_OFFSET);
+    assert.ok(constants.BITS_PER_PIXEL_OFFSET);
+    assert.ok(constants.FILE_SIZE_OFFSET);
+  });
+  it('parses header data', () => {
+    const header = new BitmapHeader(buffer);
+    header.findBitmapValues(buffer);
+    assert.equal(header.pixelOffset, 54);
+    assert.equal(header.bitsPerPixel, 24);
+    assert.equal(header.fileSize, 30054);
+  });
 });
--- tests/tranformers.test.js
+++ tests/tranformers.test.js
@@ -1,40 +1,35 @@
-const assert = require( 'assert' );
-const invTransformer = require( '../lib/invert-transformer' );
-const gsTransformer = require( '../lib/grayscale-transformer' );
-const bitmapUtilities = require( '../tests/bitmapUtilities' );
+const assert = require('assert');
+const invTransformer = require('../lib/invert-transformer');
+const gsTransformer = require('../lib/grayscale-transformer');
 
-describe( 'transformers', () => {
-  let buffer = null;
-  before( () => {
-    const path = './tests/test-bitmap.bmp';
-    buffer = bitmapUtilities.createBuffer( path );
-  } );
-  it( 'invert', () => {
+describe('transformers', () => {
 
-    const transformed = invTransformer( {
+  it('invert', () => {
+
+    const transformed = invTransformer({
       r: 34,
       g: 100,
       b: 205
-    } );
+    });
 
-    assert.deepEqual( transformed, {
+    assert.deepEqual(transformed, {
       r: 221,
       g: 155,
       b: 50
-    } );
-  } );
-  it( 'grayscale', () => {
+    });
+  });
+  it('grayscale', () => {
 
-    const transformed = gsTransformer( {
+    const transformed = gsTransformer({
       r: 34,
       g: 100,
       b: 205
-    } );
+    });
 
-    assert.deepEqual( transformed, {
+    assert.deepEqual(transformed, {
       r: 113,
       g: 113,
       b: 113
-    } );
-  } );
-} );
+    });
+  });
+});
